cfe5a5f080fa605a22abdabc9cb76a08b2df6d05
Aryanfzx <aryanfzx@gmail.com>
2025-12-01 09:29:42 +0545

    java practice monday

--- AssertInMethod.java
+++ AssertInMethod.java
@@ -6,20 +6,15 @@ public class AssertInMethod {
             throw new IllegalArgumentException("Divider 'b' must not be zero.");
         }
         return a / b;
-
     public static void main(String[] args) throws IOException {
-
-    public static void main(String[] args) {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter any two numbers:");
-        try {
-            int a = Integer.parseInt(in.readLine());
-            int b = Integer.parseInt(in.readLine());
-            System.out.println(divide(a, b));
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading input: " + e.getMessage());
-        }
+        int a = Integer.parseInt(in.readLine());
+        int b = Integer.parseInt(in.readLine());
+        System.out.println(divide(a, b));
+    }
+        System.out.println(divide(a, b));
     }
 
 }
